cdaf68f4a718944e452c0147a5ccf4387399b5e5
javiervs.dev <javiervs.dev@gmail.com>
2022-05-09 18:46:39 +0200

    Added example concatenating two functions.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # functional_programming
 Project to learn java functional programming
+
+### Function
+
+It is a functional interface that takes a single argument and returns a value.
+
+- Example:
+```java
+Function<String, Integer> func = x -> x.length();
+```
--- src/main/java/dev/javiervs/functionalprogramming/functionalinterface/FunctionMain.java
+++ src/main/java/dev/javiervs/functionalprogramming/functionalinterface/FunctionMain.java
@@ -5,16 +5,31 @@ import java.util.function.Function;
 public class FunctionMain {
     public static void main(String[] args) {
         int baseValue = 10;
-        int increment = incrementByOne(baseValue);
+
+        //Example using imperative function to increment by one
+        int incrementExample = incrementByOne(baseValue);
+        System.out.println(incrementExample);
+
+        //Example using functional interface to increment by one
+        Integer increment = incrementBy1Function.apply(baseValue);
         System.out.println(increment);
 
-        Integer incremented = incrementByOneFunction.apply(baseValue);
-        System.out.println(incremented);
+        //Example using functional interface to multiply by ten
+        int multiply = multiplyBy10Function.apply(increment);
+        System.out.println(multiply);
+
+        //Example using functional interface to increment by one and multiply by ten in one line
+        int incrementAndMultiplyInlineResult = incrementBy1Function
+                .andThen(multiplyBy10Function)
+                .apply(baseValue);
+        System.out.println(incrementAndMultiplyInlineResult);
     }
 
-    static Function<Integer, Integer> incrementByOneFunction =
+    static Function<Integer, Integer> incrementBy1Function =
             number -> number + 1;
 
+    static Function<Integer, Integer> multiplyBy10Function =
+            number -> number * 10;
     static int incrementByOne(int number) {
         return number + 1;
     }
